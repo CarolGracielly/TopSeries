0d4b6602c31aa916b862c237c46f7685680c75c8
CarolGracielly <33533323+CarolGracielly@users.noreply.github.com>
2024-09-09 13:33:57 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    { 
+        titulo : "Friends",
+        descricao : "Friends é uma série de comédia que acompanha a vida de seis amigos: Rachel, Monica, Phoebe, Joey, Chandler e Ross. Eles moram em Nova York e enfrentam juntos os desafios da vida adulta, como amor, trabalho e amizades. Com humor inteligente e personagens carismáticos, a série se tornou um fenômeno mundial, conquistando o coração de milhões de espectadores ao longo dos anos.",
+        link : "https://en.wikipedia.org/wiki/Friends",
+        tags: "amigos amor comédia"
+    },
+    { 
+        titulo : "Supernatural",
+        descricao : "Supernatural é uma série de drama sobrenatural que acompanha os irmãos Sam e Dean Winchester em suas aventuras caçando criaturas sobrenaturais. Eles viajam pelo país em um Impala de 1967, enfrentando demônios, vampiros e outras ameaças sobrenaturais.",
+        link : "https://pt.wikipedia.org/wiki/Supernatural_(série_de_televisão)",
+        tags: "irmãos demônios caçando impala"
+    },
+    { 
+        titulo : "Grey's Anatomy",
+        descricao : "Grey's Anatomy é um drama médico que acompanha a vida de Meredith Grey e de outros internos e residentes do Seattle Grace Hospital. A série explora as complexidades da vida profissional e pessoal dos médicos, além de abordar temas como amor, perda e amizade.",
+        link : "https://pt.wikipedia.org/wiki/Grey's_Anatomy",
+        tags: "residentes seattle médicos"
+    },
+];
+
